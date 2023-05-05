0d0a8063c2158a0caf223c70e47be955e5372d11
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-05-04 19:09:45 -0700

    Uses numpy to preprocess test images into binary images

--- image-read.py
+++ image-read.py
@@ -1,12 +1,28 @@
 from PIL import Image
 import numpy as np
-import cv2
 
+img = np.array(Image.open("./test_images/letter_C.jpg").convert('L'))
+# img = np.array(Image.open("./rgbrender_10234.png").convert('L'))
+print(img)
+thresh = 200
+img_bool = img > thresh
+print(img_bool)
+maxval = 255
+img_bin = (img > thresh) * maxval
+print(img_bin)
+img_bin_keep = (img > thresh) * img
+print(img_bin_keep)
+
+filename = "test_image"
+Image.fromarray(np.uint8(img_bin_keep)).save(f'./{filename}_read.png')
 
 # # Reads file and converts to numpy array
 # filename = "rgbrender_13456"
 
+
 # img = Image.open(f"./{filename}.png")
+
+##########################################################
 # numpydata = np.asarray(img)
 
 # print(numpydata)
