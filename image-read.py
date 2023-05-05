467c745f68b25c496785bcbe524a055d6138e3d2
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-05-04 18:22:55 -0700

    installs and imports opencv for python

--- image-read.py
+++ image-read.py
@@ -1,19 +1,22 @@
 from PIL import Image
 import numpy as np
-# from numpy import asarray
-filename = "rgbrender_13456"
-
-img = Image.open(f"./{filename}.png")
-numpydata = np.asarray(img)
-
-print(numpydata)
-
-# Writes image to a png file
+import cv2
 
 
-def write_image(image_array):
-    img = Image.fromarray(image_array)
-    img.save(f'{filename}_read.png')
+# # Reads file and converts to numpy array
+# filename = "rgbrender_13456"
+
+# img = Image.open(f"./{filename}.png")
+# numpydata = np.asarray(img)
+
+# print(numpydata)
+
+# # Writes image to a png file
 
 
-write_image(numpydata)
+# def write_image(image_array):
+#     img = Image.fromarray(image_array)
+#     img.save(f'{filename}_read.png')
+
+
+# write_image(numpydata)
